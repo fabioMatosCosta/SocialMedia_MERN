c5221e96972de4af092de7b270828ca6736f3f3e
Fábio Costa <fabiodanielmc@gmail.com>
2024-02-12 22:45:30 +0100

    fixed minor bugs with register and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,7 @@ export const login =  async (req, res) => {
         const user = await User.findOne({ email:email });
         if (!user) return res.status(400).json({msg: "User does not exist"});
 
-        const isMatch = await bcrypt.compare( password, use.password );
+        const isMatch = await bcrypt.compare( password, user.password );
         if (!isMatch) return res.status(400).json({msg: "Invalid credentials"});
 
         const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
